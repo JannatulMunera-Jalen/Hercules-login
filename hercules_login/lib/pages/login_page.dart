4cd32062ef86e7c5d1b15cb09c61c6667330d4b0
JannatulMunera-Jalen <jannatulmunera.jalen@students.mq.edu.au>
2023-01-30 16:18:55 +1100

    LOGIN PAGE COMPLETED
    login page updated, it doesnt do anything yet. but looks pretty

--- hercules_login/lib/pages/login_page.dart
+++ hercules_login/lib/pages/login_page.dart
@@ -28,59 +28,70 @@ class _LoginPageState extends State<LoginPage> {
             ),
             SafeArea(
               child: Container(
-                  //this will be the login form
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  //this is the login form
                   child: Column(
-                children: [
-                  Text(
-                    'Hercules Group',
-                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 30.0),
-                  Form(
-                      key: _formKey,
-                      child: Column(
-                        children: [
-                          TextField(
-                            decoration: ThemeHelper().textInputDecoration(
-                                'User Name', 'Enter your User Name'),
-                          ),
-                          SizedBox(height: 30.0),
-                          TextField(
-                            obscureText: true,
-                            decoration: ThemeHelper().textInputDecoration(
-                                'Password', 'Enter your Password'),
-                          ),
-                          SizedBox(height: 15.0),
-                          Container(
-                            child: Text('Forgot Password?'),
-                          ),
-                          Container(
-                            decoration:
-                                ThemeHelper().buttonBoxDecoration(context),
-                            child: ElevatedButton(
-                              style: ThemeHelper().buttonStyle(),
-                              child: Padding(
-                                padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
-                                child: Text(
-                                  'Sign In'.toUpperCase(),
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
+                    children: [
+                      Text(
+                        'Hercules Group',
+                        style: TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Signin into your account',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      SizedBox(height: 30.0),
+                      Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextField(
+                                decoration: ThemeHelper().textInputDecoration(
+                                    'User Name', 'Enter your User Name'),
+                              ),
+                              SizedBox(height: 30.0),
+                              TextField(
+                                obscureText: true,
+                                decoration: ThemeHelper().textInputDecoration(
+                                    'Password', 'Enter your Password'),
+                              ),
+                              SizedBox(height: 15.0),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 0, 10, 20),
+                                alignment: Alignment.topRight,
+                                child: Text('Forgot your Password?'),
+                              ),
+                              Container(
+                                decoration:
+                                    ThemeHelper().buttonBoxDecoration(context),
+                                child: ElevatedButton(
+                                  style: ThemeHelper().buttonStyle(),
+                                  child: Padding(
+                                    padding:
+                                        EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                    child: Text(
+                                      'Sign In'.toUpperCase(),
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  //after successful login, we will redirect to profile page
                                 ),
                               ),
-                              onPressed: () {},
-                              //after successful login, we will resirect to profile page
-                            ),
-                          ),
-                          Container(
-                            child: Text(
-                                "Don't have an account? Sign up here!"), //Now style the forms. creating a theme helper class...
-                          ),
-                        ],
-                      )),
-                ],
-              )),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                                child: Text(
+                                    "Don't have an account? Sign up here!"), //Now style the forms. creating a theme helper class...
+                              ),
+                            ],
+                          )),
+                    ],
+                  )),
             )
           ],
         ),
