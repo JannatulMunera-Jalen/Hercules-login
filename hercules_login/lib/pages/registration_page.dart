d08e41eed816e74f1a4cfdf6fc2488fe11c56302
JannatulMunera-Jalen <jannatulmunera.jalen@students.mq.edu.au>
2023-02-22 14:20:45 +1100

    Revert "Registration Page"
    This reverts commit 35ea4a78b85e01e9443271fc01d0d32f37284793.

--- hercules_login/lib/pages/registration_page.dart
+++ hercules_login/lib/pages/registration_page.dart
@@ -20,44 +20,6 @@ class _RegistrationPageState extends State<RegistrationPage> {
   @override
   Widget build(BuildContext Context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Registration Form",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        elevation: 0.5,
-        iconTheme: IconThemeData(color: Colors.white),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[
-                Theme.of(context).primaryColor,
-                Theme.of(context).colorScheme.secondary,
-              ])),
-        ),
-        actions: [
-          Container(
-            margin: EdgeInsets.only(top: 16, right: 16),
-            child: Stack(
-              children: <Widget>[
-                Icon(Icons.notifications),
-                Positioned(
-                  right: 0,
-                  child: Container(
-                    padding: EdgeInsets.all(1),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(6),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Stack(
